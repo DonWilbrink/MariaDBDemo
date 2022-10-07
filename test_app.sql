f09893e29a54c4ff8fd3a6655d3c0a86eb100e0a
Don Wilbrink <don.wilbrink@gmail.com>
2022-10-07 22:48:01 +0200

    SQL file aangepast

--- test_app.sql
+++ test_app.sql
@@ -1,24 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Gegenereerd op: 02 okt 2022 om 19:25
--- Serverversie: 8.0.30-0ubuntu0.22.04.1
--- PHP-versie: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test_app`
+-- Database: `platen`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +12,7 @@ CREATE TABLE `discs` (
   `Titel` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'not null',
   `Artiest` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Jaar` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 --
 -- Gegevens worden geëxporteerd voor tabel `discs`
@@ -56,7 +36,7 @@ CREATE TABLE `tracks` (
   `TrackTitle` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'not null',
   `TrackArtiest` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `Tijdsduur` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tracks`
@@ -96,7 +76,3 @@ ALTER TABLE `discs`
 ALTER TABLE `tracks`
   ADD PRIMARY KEY (`TID`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
